3aa602235e01f5ee181d2be4f1669e2d67365603
RaymondWC <raymond.w.jahn@gmail.com>
2020-10-07 15:34:23 +0900

    Flex Cable Design + Folder Reorganization
    1. Design for flex cables finished
    2. Folders reorganized/names changed

--- DAQ Software/USB Comm.py
+++ DAQ Software/USB Comm.py
@@ -1,15 +1,19 @@
-import datetime
 import serial
+import datetime
+import threading
+import time
 
-serialPort = serial.Serial(port = "COM6", baudrate = 9600, bytesize = 8, timeout = 0, stopbits = serial.STOPBITS_ONE)
+frame_w = int(768 / 2)
+frame_h = int(592 / 2)
+frame = bytearray(100) # frame_w * frame_h)
+
+# Make sure to run this script before connecting the MCU USB (Keep the UART USB connected, so the port doesn't close.)
+# Max baudrate = 3000000
+serialPort = serial.Serial(port = "COM6", baudrate = 3000000, bytesize = 8, timeout = None, stopbits = serial.STOPBITS_ONE)
 t_before = datetime.datetime.now()
 
-serialString = ""
-if (serialPort.in_waiting > 0):
-    serialString = serialPort.readline()
+ppf = serialPort.readinto(frame)
 
 t_after = datetime.datetime.now()
-t_diff = t_after - t_before
+t_diff = (t_after - t_before).total_seconds()
 t_msec = t_diff.microseconds
-
-print(serialString, t_msec, len(serialString)/t_msec)
